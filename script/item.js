6a858b7f7c3f8897480e952cba9d352250c7dd64
PrachiKarande <166218280+PrachiKarande@users.noreply.github.com>
2025-07-22 19:30:48 +0530

    Add files via upload

---
+++ script/item.js
@@ -0,0 +1,64 @@
+const items = [
+    {
+        id: '001',
+        image: 'himage/1.jpg',
+        company: 'Carlton London',
+        item_name: 'Rhodium-Plated CZ Floral Studs',
+        original_price: 1045,
+        current_price: 606,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+        rating: {
+            stars: 4.5,
+            count: 1400,
+        },
+    },
+    {
+        id: '002',
+        image: 'himage/2.jpg',
+        company: 'CUKOO',
+        item_name: 'Women Padded Halter Neck Swimming Dress',
+        original_price: 2599,
+        current_price: 1507,
+        discount_percentage: 42,
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+        rating: {
+            stars: 4.3,
+            count: 24,
+        },
+    },
+    {
+        id: '003',
+        image: 'himage/3.jpg',
+        company: 'NUEVOSDAMAS',
+        item_name: 'Women Red & White Printed A-Line Knee-Length Skirts',
+        original_price: 1599,
+        current_price: 495,
+        discount_percentage: 69,
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+        rating: {
+            stars: 4.1,
+            count: 249,
+        },
+    },
+    
+    {
+        id: '004',
+        image: 'himage/6.jpg',
+        company: 'Nike',
+        item_name: 'Men ReactX Running Shoes',
+        original_price: 14995,
+        current_price: 14995,
+        discount_percentage: 0,
+        return_period: 14,
+        delivery_date: '10 Oct 2023',
+        rating: {
+            stars: 0.0,
+            count: 0,
+        },
+    
+    }
+];
